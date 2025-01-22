85fcd5dd37d5c34438555eecd4377d0d9f35e2b6
aandoney <angel_andoney@live.com.mx>
2025-01-21 20:45:41 -0600

    added routing

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,11 +1,9 @@
-import { useEffect, useState } from "react"
-import { Product } from "../models/product";
-import Catalog from "../../features/catalog/Catalog";
+import { useState } from "react"
 import { Box, Container, createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import NavBar from "./NavBar";
+import { Outlet } from "react-router-dom";
 
-function App() {
-  const [products, setProducts] = useState<Product[]>([]);
+function App() {  
   const [darkMode, setDarkMode] = useState(false);
   const palleteType = darkMode ? 'dark' : 'light';
   const theme = createTheme({
@@ -16,12 +14,6 @@ function App() {
       }
     }
   })
-
-  useEffect(() => {
-    fetch('https://localhost:5001/api/products')
-      .then(response => response.json())
-      .then(data => setProducts(data))
-  }, []);
 
   const toggleDarkMode = () => {
     setDarkMode(!darkMode);
@@ -41,7 +33,7 @@ function App() {
         }}
       >
         <Container maxWidth='xl' sx={{ mt: 8 }}>
-          <Catalog products={products} />
+          <Outlet />
         </Container>
       </Box>
     </ThemeProvider>
